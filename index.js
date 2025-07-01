2377507e476015505d62888fc3b8b7730cc816e9
Beryl-742 <Berylawuor742@gmail.com>
2025-07-01 15:57:35 +0300

    Js-looping

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Code your solutions in this file
+//writecards()Function (Using a for Loop)
+function writeCards(names, eventName) {
+  const messages = [];
+
+  for (let i = 0; i < names.length; i++) {
+    const message = `Thank you, ${names[i]}, for the wonderful ${eventName} gift!`;
+    messages.push(message);
+  }
+
+  return messages;
+}
+// count down() Function (Uisng a while loop)
+function countDown(startingNumber) {
+  while (startingNumber >= 0) {
+    console.log(startingNumber);
+    startingNumber--;
+  }
+}
